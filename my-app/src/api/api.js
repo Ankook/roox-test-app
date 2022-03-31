6c2df4b94e55668adf13c677e25d593586803cea
Ankook <52046510+Ankook@users.noreply.github.com>
2022-04-01 02:14:40 +0300

    starting to create axios api folder

---
+++ my-app/src/api/api.js
@@ -0,0 +1,14 @@
+import * as axios from "axios";
+
+const instance = axios.create({
+  baseURL:'https://jsonplaceholder.typicode.com/users/'
+})
+
+export const usersAPI = {
+  getUsers() {
+    return instance.get().then(response => response.data) //Возможно, нужно написать response.json
+  },
+  getUser(userId) {
+    return instance.get(`${userId}`).then(response =>response.data);
+  }
+}

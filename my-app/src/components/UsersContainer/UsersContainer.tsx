6c3c2fe4bf916256e55553c13afb1374c2fbd123
Ankook <52046510+Ankook@users.noreply.github.com>
2022-05-02 09:35:39 +0300

    removing antipattern code

--- my-app/src/components/UsersContainer/UsersContainer.tsx
+++ my-app/src/components/UsersContainer/UsersContainer.tsx
@@ -2,6 +2,7 @@ import React, {  useState, useEffect } from 'react';
 import styles from "./usersContainer.module.scss"
 import Users from "./Users";
 import { usersAPI } from '../../api/api';
+import { useAppSelector } from '../../app/hooks';
 
 
 
@@ -10,9 +11,11 @@ type UsersContainerType = {
   sortType: string
 }
 
-const UsersContainer:React.FC<UsersContainerType> = ({ sortType }) => {
-  console.log("Значение пропса sortType");
-  console.log(sortType);
+const UsersContainer: React.FC<UsersContainerType> = ({ }) => {
+  
+  const users = useAppSelector(state => state.users.users);
+  console.log(users);
+
   
   const [data, setData] = useState(null);
   const [sortEvent, setSortEvent] = useState(0);
@@ -32,49 +35,6 @@ const UsersContainer:React.FC<UsersContainerType> = ({ sortType }) => {
     if (a.company.name === b.company.name) return 0;
     if (a.company.name < b.company.name) return -1;
   } 
-
-  
-
-  useEffect(() => {
-    usersAPI.getUsers()
-      .then((actualData: any) => {
-        console.log("Обращаемся к API");
-        console.log(actualData);
-        setData(actualData);
-        setUsersCount(actualData.length);
-        setError(null);
-      })
-      .catch((err: any) => {
-        console.log(err.message);
-      })
-      .finally(() => {
-        setLoading(false)
-      });
-  }, []);
-
-  useEffect(() => {
-    //console.log("Значение data");
-    //console.log(data);
-    if (data !== null) {
-      //console.log("Значение пропса sortType внутир useEffect");
-      //console.log(sortType);
-      //console.log("Тип sortType внутри useEffect");
-      //console.log(typeof sortType);
-      if (sortType == "city") {
-        console.log("Сортируем пользователей по городу");
-        setData(data.sort(sortByCity));
-        setSortEvent(sortEvent + 1);
-        console.log(sortEvent);
-        //console.log(data);
-      } else if (sortType == "name") {
-        console.log("Сортируем пользоватеей по назаванию компании");
-        setData(data.sort(sortByCompanyName));
-        setSortEvent(sortEvent + 1);
-        console.log(sortEvent);
-        //console.log(data);
-      }
-    }
-  }, [sortType])
 
   return (
     <div className={styles.users}>
